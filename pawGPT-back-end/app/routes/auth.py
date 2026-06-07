ead7a80e92c9f074f4bcddab883350f42abef77f
gobind-singh21 <sgobind577@gmail.com>
2026-06-08 04:15:38 +0530

    added logout end point

--- pawGPT-back-end/app/routes/auth.py
+++ pawGPT-back-end/app/routes/auth.py
@@ -104,3 +104,9 @@ async def refresh_session(response: Response, refresh_token: Optional[str] = Coo
 	)
     
     return {"status": "session_refreshed", "user": username}
+
+@router.post("/logout")
+async def logout(response: Response):
+    response.delete_cookie(key="access_token", httponly=True, samesite="strict", secure=False)
+    response.delete_cookie(key="refresh_token", httponly=True, samesite="strict", secure=False)
+    return {"message": "Logged out successfully"}
